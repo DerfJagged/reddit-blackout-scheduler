34ce9444839515ce94014c80d3176b7f37749bc7
Derf <24844708+DerfJagged@users.noreply.github.com>
2023-06-07 11:43:52 -0500

    Update variables.py

--- variables.py
+++ variables.py
@@ -1,2 +1,9 @@
-SUBREDDITS = ['DerfJagged','DerfCSS']
-DESCRIPTION = ' [is now private. Click here to find out why we have gone dark](https://www.theverge.com/2023/6/5/23749188/reddit-subreddit-private-protest-api-changes-apollo-charges)' 
+subreddits = ['DerfJagged','DerfCSS']
+description = ' [is now private. Click here to find out why we have gone dark](https://old.reddit.com/r/Save3rdPartyApps/comments/13yh0jf/dont_let_reddit_kill_3rd_party_apps/)' 
+posts = [
+{"title": " will go dark from 12-14 June in protest against Reddit API price changes",
+"link": "linkurl",
+"date": "6,8"}
+]
+blackout_date = "6,9"
+blackout_end_date = "6,10"
