ff7018cf1a197408feb332fd468b970ad9d248c1
Derf <24844708+DerfJagged@users.noreply.github.com>
2023-06-07 17:22:21 -0500

    Update variables.py

--- variables.py
+++ variables.py
@@ -1,6 +1,6 @@
 subreddits = ['DerfJagged','DerfCSS']
 description = ' [is now private. Click here to find out why we have gone dark](https://old.reddit.com/r/Save3rdPartyApps/comments/13yh0jf/dont_let_reddit_kill_3rd_party_apps/)' 
-posts = [
+post = [
 {
   "title": " will go dark from 12-14 June in protest against Reddit API price changes",
   "link": "https://old.reddit.com/r/Save3rdPartyApps/comments/13yh0jf/dont_let_reddit_kill_3rd_party_apps/",
