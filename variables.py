2876e9426767cfca527bb68792c1472336e27714
Derf <24844708+DerfJagged@users.noreply.github.com>
2023-06-07 10:22:26 -0500

    Update and rename postqueue.py to variables.py

---
+++ variables.py
@@ -0,0 +1,2 @@
+SUBREDDITS = ['DerfJagged','DerfCSS']
+DESCRIPTION = ' [is now private. Click here to find out why we have gone dark](https://www.theverge.com/2023/6/5/23749188/reddit-subreddit-private-protest-api-changes-apollo-charges)' 
