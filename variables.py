baea99a5aa7a976323fa12b15d9fe7b68866db26
Derf <24844708+DerfJagged@users.noreply.github.com>
2023-06-07 19:14:30 -0500

    Update variables.py

--- variables.py
+++ variables.py
@@ -1,13 +1,17 @@
+#Subreddits to post announcement to / blackout
 subreddits = ['DerfJagged','DerfCSS']
+
+#Temporary description users see when they visit during the blackout
 description = ' [is now private. Click here to find out why we have gone dark](https://old.reddit.com/r/Save3rdPartyApps/comments/13yh0jf/dont_let_reddit_kill_3rd_party_apps/)' 
-post = [
-{
-  "title": " will go dark from 12-14 June in protest against Reddit API price changes",
+
+#Announcement Post
+post = {
+  "title": " will go dark from June 12-14 in protest against Reddit API price changes",
   "link": "https://old.reddit.com/r/Save3rdPartyApps/comments/13yh0jf/dont_let_reddit_kill_3rd_party_apps/",
   "sticky": False,
   "distinguish": True
 }
-]
+
 #Credentials
 reddit = praw.Reddit(
 	client_id='',          # From the app prefs page on reddit
